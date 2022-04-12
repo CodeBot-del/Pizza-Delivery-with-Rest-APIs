ecc5c31adab229e081681c8c0b56a95beb2047b9
gracie2 <stevenmanangu360@gmail.com>
2022-04-12 23:28:01 +0300

    creating user model

--- authentication/models.py
+++ authentication/models.py
@@ -1,3 +1,15 @@
-from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.base_user import BaseUserManager
+from django.utils.translation import ugettext_lazy as _
+
 
 # Create your models here.
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, password, **extra_fields):
+        if not email:
+            raise ValueError(_('Email should be provided'))
+        
+        email=self.normalize_email(email)
+    
+    def create_superuser(self, email, password, **extra_fields):
+        pass
