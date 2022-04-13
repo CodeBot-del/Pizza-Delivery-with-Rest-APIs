1c9216339a98b9e9b36c0085e85440a83ea0f9e3
gracie2 <stevenmanangu360@gmail.com>
2022-04-13 15:11:06 +0300

    creating the models

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
 # Create your models here.
+
+User = get_user_model()
+
+class Order(models.Model):
+    
+    SIZES=(
+        ('SMALL', 'small'),
+        ('MEDIUM', 'medium'),
+        ('LARGE', 'large'),
+        ('EXTRA_LARGE', 'extraLarge'),
+    )
+    
+    ORDER_STATUS=(
+        ('PENDING', 'pending'),
+        ('IN_TRANSIT', 'inTransit'),
+        ('DELIVERED', 'delivered'),
+    )
+    
+    customer = models.ForeignKey(User, on_delete=models.CASCADE) #when you delete a user, all the orders related to the user will be deleted also
+    # other fields 
+    size=models.CharField(max_length=20, choices=SIZES)
+    order_status = models.CharField(max_length=20, choices=ORDER_STATUS)
